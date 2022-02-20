f89e5248e7d9e6ad248bcad488c77d881ebab145
Estevão Dias <estevaodrdias@gmail.com>
2022-02-20 10:56:18 -0300

    :recycle: Rename parameter

--- src/main/java/com/estevaodias/geekstore/infrastructure/security/web/WebSecurityConfiguration.java
+++ src/main/java/com/estevaodias/geekstore/infrastructure/security/web/WebSecurityConfiguration.java
@@ -31,7 +31,7 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(final HttpSecurity http) throws Exception {
     http.csrf().disable()
         .cors().and()
-        .authorizeRequests(e -> e
+        .authorizeRequests(a -> a
             .antMatchers(POST, "/users").permitAll()
             .anyRequest().authenticated()
         )
