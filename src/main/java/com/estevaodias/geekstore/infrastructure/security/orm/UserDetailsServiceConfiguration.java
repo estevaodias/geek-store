eee4d8af884878a329237c72f680830d85417c59
Estevão Dias <estevaodrdias@gmail.com>
2022-02-20 10:52:32 -0300

    :truck: Adjusting boundaries

--- src/main/java/com/estevaodias/geekstore/infrastructure/security/orm/UserDetailsServiceConfiguration.java
+++ src/main/java/com/estevaodias/geekstore/infrastructure/security/orm/UserDetailsServiceConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static java.util.Collections.emptyList;
 
@@ -12,6 +13,11 @@ class UserDetailsServiceConfiguration {
 
   @Bean
   public UserDetailsService userDetailsService() {
-    return username -> new User("", "", emptyList());
+    return username -> {
+      final var user = userRepository.findByUsername(username)
+          .orElseThrow(() -> new UsernameNotFoundException(username));
+
+      return new User(user.getUsername(), "", emptyList());
+    };
   }
 }
