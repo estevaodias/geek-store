cd237fe36195d49eec63f436cf0a442b10ad69bf
Estevão Dias <estevaodrdias@gmail.com>
2022-02-20 11:00:31 -0300

    :truck: Adjusting boundaries

--- src/main/java/com/estevaodias/geekstore/infrastructure/security/web/CorsConfigurationSourceConfiguration.java
+++ src/main/java/com/estevaodias/geekstore/infrastructure/security/web/CorsConfigurationSourceConfiguration.java
@@ -1,4 +1,4 @@
-package com.estevaodias.geekstore.infrastructure.security.beans;
+package com.estevaodias.geekstore.infrastructure.security.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
